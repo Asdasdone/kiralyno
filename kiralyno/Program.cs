89d74c40e34cd278970c2fb6c3f573e2f60e54a5
Asdasdone <tohm69@gmail.com>
2020-09-16 09:29:47 +0200

    4. feladat

--- kiralyno/Program.cs
+++ kiralyno/Program.cs
@@ -8,17 +8,37 @@ namespace kiralyno
 {
      class tabla
     {
-       private char[,] t = new char[8,8];
-        private char urescella = new char();
-        private int uresoszlopok = new int();
-        private int uresoszsorok = new int();
+        private char[,] t;
+        private char urescella;
+        private int uresoszlopok;
+        private int uresoszsorok;
         public tabla(char a)
         {
-
+            t = new char[8, 8];
+            urescella = a;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    t[i, y] = a;
+                }
+            }
         }
-        public void elhelyez()
+        public void elhelyez(int n)
         {
-
+            Random rnd = new Random();
+            int a=new int();
+            int b = new int();
+            while (n!=0)
+            {
+                a = rnd.Next(0, 8);
+                b = rnd.Next(0, 8);
+                if (t[a,b]!='K')
+                {
+                    t[a, b] = 'K';
+                    n--;
+                }
+            }
         }
         public void fajbair()
         {
@@ -26,7 +46,14 @@ namespace kiralyno
         }
         public void megjelenit()
         {
-
+            for (int i = 0; i < 8; i++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Console.Write(t[i,y]+"\t");
+                }
+                Console.WriteLine("\n\n");
+            }
         }
         public int uresoszlop()
         {
@@ -42,6 +69,10 @@ namespace kiralyno
     {
         static void Main(string[] args)
         {
+            tabla asd = new tabla('#');
+            asd.elhelyez(4);
+            asd.megjelenit();
+            Console.ReadKey();
         }
     }
 }
