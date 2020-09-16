e55f5725947876ad8b272a605240f34faa384ae0
Asdasdone <tohm69@gmail.com>
2020-09-16 08:29:10 +0200

    Add project files.

---
+++ kiralyno/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiralyno
+{
+     class tabla
+    {
+       private char[,] t = new char[8,8];
+        private char urescella = new char();
+        private int uresoszlopok = new int();
+        private int uresoszsorok = new int();
+        public tabla(char a)
+        {
+
+        }
+        public void elhelyez()
+        {
+
+        }
+        public void fajbair()
+        {
+
+        }
+        public void megjelenit()
+        {
+
+        }
+        public int uresoszlop()
+        {
+            return 0;
+        }
+        public int uressor()
+        {
+            return 0;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
